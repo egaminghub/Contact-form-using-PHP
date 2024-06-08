a16fdefbafdcd5c95e89ea257b96c4c1650f36ef
Rishav Roy <152189244+egaminghub@users.noreply.github.com>
2024-06-08 12:16:41 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,29 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    $con = mysqli_connect($server, $username, $password);
+    if(!$con){
+        die("Connection failed due to". $mysqli_connect_error());
+    }
+    // echo"Succcess";
+    $name= $_POST['name'];
+    $email= $_POST['email'];
+    $phone= $_POST['phone'];
+    $desc= $_POST['desc'];
+
+    $sql = "INSERT INTO `dbfform`.`contact-us` ( `Name`, `Email`, `Phone`, `Message`, `dt`) VALUES ('$name', '$email', '$phone', '$desc', current_timestamp());";
+    if($con->query($sql)== true){
+        // echo"Successfully Inserted";
+        
+    }
+    else {
+        echo"Error: $sql $con->error";
+    }
+    $con->close();
+    echo" ";
+?>
+
+<h1>Form Submitted Succrssfully</h1>
+<button><a href="index.html">Go Back to HomePage</a></button>
